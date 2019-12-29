a48f0fbd8cbaf4f4adc29a26f0a99d70367ea999
Adrian Riza <rizariza19@gmail.com>
2019-12-30 01:02:21 +0200

    removed port for express

--- server.js
+++ server.js
@@ -39,4 +39,4 @@ const server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, () => console.log(`API running on ${port}`));
+//server.listen(port, () => console.log(`API running on ${port}`));
