57f85b9b3bbb07f2d22d0ff4c0fadb39a56806e2
Adrian Riza <rizariza19@gmail.com>
2020-01-13 17:38:44 +0200

    swich to node start

--- server.js
+++ server.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const proxy = require('express-http-proxy');
+const cors = require('cors');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+// Parsers for POST data
+app.use(bodyParser.json({limit: '20mb'}));
+app.use(bodyParser.urlencoded({ extended: false, limit: '20mb' }));
 
-app.get('/*', function(req,res) {
+app.use(cors());
 
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist/vaccine-ui')));
+
+// Set our api routes proxy to point to spring boot server
+app.use('/server', proxy('https://vaccine-bot.herokuapp.com/'));
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/vaccine-ui/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+/**
+ * Get port from environment and store in Express.
+ */
+const port = '4200';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on ${port}`));
